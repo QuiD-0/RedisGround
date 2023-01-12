7e7f4c50288fbc61f69f1257e3866a0373a9c362
QuiD-0 <wodnd101@naver.com>
2023-01-12 16:05:46 +0900

    init spring

---
+++ src/main/java/com/quid/redisGround/RedisGroundApplication.java
@@ -0,0 +1,13 @@
+package com.quid.redisGround;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedisGroundApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RedisGroundApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/quid/redisGround/RedisGroundApplicationTests.java
@@ -0,0 +1,13 @@
+package com.quid.redisGround;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedisGroundApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
